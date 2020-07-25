047dff71f68f45d2a584ac33e6de26d8fd9ef6e6
uniqname <lijame@umich.edu>
2020-07-25 17:29:00 -0400

    back

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -1,8 +1,23 @@
 import flask 
+from flask_sqlalchemy import SQLAlchemy
+
+
 app = flask.Flask("__main__")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////test.db'
+db = SQLAlchemy(app)
+
+class Streamer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    title = db.Column(db.String(80),  nullable=False)
+    avi = db.Column(db.String(20),  nullable=False, default ='default.jpg')
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 @app.route("/")
 def my_index():
-    flask.render_template("index.html", token="Mentii"))
+    flask.render_template("index.html", token="Mentii")
 
 app.run(debug=True)
+
