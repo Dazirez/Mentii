9ea688e95fa4c874d298fd58b94df8437b2414b3
Daniel Zheng <danielzheng@Daniels-MacBook-Pro-3.local>
2020-07-25 14:39:56 -0400

    added JS pages

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Grid, Button } from "@material-ui/core";
+import Header from "./Header";
+import Content from "./Content";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Mentii</h1>
+            <Button> Log in</Button>
+            <Button>Get Started</Button>
+        </div>
+    );
 }
 
 export default App;
